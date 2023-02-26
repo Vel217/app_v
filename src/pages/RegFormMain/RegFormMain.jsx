f23834409f06851b0dba115097976d3f2fd67447
Mikhail Beliakov <miklebel97@gmail.com>
2023-02-26 10:26:44 +0200

    fix fetch request data

--- src/pages/RegFormMain/RegFormMain.jsx
+++ src/pages/RegFormMain/RegFormMain.jsx
@@ -5,87 +5,89 @@ import HaveAcc from "./HaveAcc";
 import Title from "../../components/Title/Title";
 
 import classes from "./RegFormMain.module.css";
+import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 
 function RegFormMain() {
   // const navigate = useNavigate();
-  function click() {
-    const first_name = document.getElementsByName("first_name").value;
-    const second_name = document.getElementsByName("second_name").value;
-    const login = document.getElementsByName("login").value;
-    const email = document.getElementsByName("email").value;
-    const password = document.getElementsByName("password").value;
-    const phone = document.getElementsByName("phone").value;
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [login, setLogin] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const click = async () => {
     const host = "https://ya-praktikum.tech";
-    fetch(`$(host)/api/v2/auth/signup`, {
+    const url = `${host}/api/v2/auth/signup`;
+    const data = {
+      first_name: firstName,
+      second_name: secondName,
+      login: login,
+      email: email,
+      phone: phone,
+      password: password,
+    };
+    const response = await fetch(url, {
       method: "POST",
       mode: "cors",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify({
-        first_name: first_name,
-        second_name: second_name,
-        login: login,
-        email: email,
-        phone: phone,
-        password: password,
-      }),
-    })
-      .then((response) => response.text())
-      .then((data) => {
-        fetch(`$(host)/api/v2/auth/user`, {
-          method: "GET",
-          mode: "cors",
-        })
-          .then((r) => r.json())
-          .then((data) => {
-            console.log("user", data);
-          });
-      });
-  }
+      body: JSON.stringify(data),
+    });
+
+    console.log(data);
+
+    console.log(response);
+  };
 
   return (
     <>
       <div>
         <Title>Create an account:</Title>
         <Input
-          id="login"
+          value={email}
+          onChange={setEmail}
           type="email"
           placeholder="any.any@mail.com"
           title="Email"
           name="email"
         />
         <Input
-          id="login"
+          value={login}
+          onChange={setLogin}
           type="text"
           placeholder="LoginName"
           title="Login"
           name="login"
         />
         <Input
-          id="firstName"
+          value={firstName}
+          onChange={setFirstName}
           type="text"
           placeholder="First Name"
           title="First Name"
           name="first_name"
         />
         <Input
-          id="lastName"
+          value={secondName}
+          onChange={setSecondName}
           type="text"
           placeholder="Last Name"
           title="Last Name"
           name="second_name"
         />
         <Input
-          id="phoneNumber"
+          value={phone}
+          onChange={setPhone}
           type="tel"
           placeholder="+9(999)999-99-99"
           title="Phone"
           name="phone"
         />
         <Input
-          id="passCreate"
+          value={password}
+          onChange={setPassword}
           type="password"
           placeholder="*************"
           title="Password"
@@ -95,7 +97,7 @@ function RegFormMain() {
 
       <div className={classes.buttonBox}>
         <Button
-          onClick={() => click()}
+          onClick={click}
           // onClick={() => navigate("/chat")}
           isOrange={true}
         >
